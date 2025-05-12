240a986dcee474bfc868d4cd97689cd5f3d0cf4f
bogdflor <112245361+bogdflor@users.noreply.github.com>
2025-05-12 10:51:06 +0300

    Update appium.test.js

--- test/appium.test.js
+++ test/appium.test.js
@@ -1,7 +1,7 @@
 // test/appium.test.js
 import wd from 'wd';
 
-const driver = wd.promiseChainRemote("http://localhost:4723/wd/hub");
+const driver = wd.promiseChainRemote("http://127.0.0.1:4723/wd/hub");
 
 await driver.init({
   platformName: "Android",
